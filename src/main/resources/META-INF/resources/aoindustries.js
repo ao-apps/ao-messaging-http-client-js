e2c00a704a73876a0638b381ff583b61cd546fbc
AO Industries, Inc. <support@aoindustries.com>
2016-10-09 22:48:15 -0500

    Set top comment correct for this submodule name.

--- src/main/resources/META-INF/resources/aoindustries.js
+++ src/main/resources/META-INF/resources/aoindustries.js
@@ -1,10 +1,24 @@
-/**
- * JavaScript API provided by AO Industries, Inc.
- *
- * Copyright (C) 2014  AO Industries, Inc.
+/*
+ * ao-messaging-http-client-js - JavaScript client for asynchronous bidirectional messaging over HTTP.
+ * Copyright (C) 2014, 2016  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
+ *
+ * This file is part of ao-messaging-http-client-js.
+ *
+ * ao-messaging-http-client-js is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-messaging-http-client-js is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-messaging-http-client-js.  If not, see <http://www.gnu.org/licenses/>.
  */
 if(typeof aoindustries === 'undefined') aoindustries = {};
 
