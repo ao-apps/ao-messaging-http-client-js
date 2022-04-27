27d04b274d2ccf5cdf0e9159bc7308be43aa9fbb
AO Industries, Inc <support@aoindustries.com>
2022-04-26 23:59:01 -0500

    Activated org.openrewrite.java.cleanup.Cleanup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 /*
  * ao-messaging-http-client-js - JavaScript client for asynchronous bidirectional messaging over HTTP.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -21,4 +21,4 @@
  * along with ao-messaging-http-client-js.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.aoapps.messaging.http.client.js {
-}
+} // TODO: Avoiding rewrite-maven-plugin-4.22.2 truncation
